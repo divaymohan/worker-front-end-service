adac4828bd1641c675994056f7bb0d48f18b86f2
divaymohan <divaymohan05@gmail.com>
2020-06-14 12:58:47 +0530

    adding state and handles to the form component

--- worker-frontend/src/components/login.jsx
+++ worker-frontend/src/components/login.jsx
@@ -1,9 +1,55 @@
 import React, { Component } from "react";
+import FooterBar from "./footer/footer";
 
 class Login extends Component {
-  state = {};
+  state = {
+    account: {
+      username: " ",
+      password: " ",
+    },
+  };
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("submitted page..!!");
+  };
+  handleChange = (e) => {
+    console.log("changed the state");
+  };
   render() {
-    return <h1>Login</h1>;
+    return (
+      <React.Fragment>
+        <div className="container">
+          <div className="card m-md-5 p-3">
+            <div className="m-5">
+              <form onSubmit={this.handleSubmit}>
+                <div className="form-group">
+                  <lable for="username">Username : </lable>
+                  <input
+                    type="emial"
+                    onChange={this.handleChange}
+                    className="form-control"
+                    id="username"
+                    autoFocus
+                  />
+                </div>
+                <div className="form-group">
+                  <lable for="password">Password : </lable>
+                  <input
+                    type="pssword"
+                    className="form-control"
+                    id="password"
+                  />
+                </div>
+                <button type="submit" className="btn btn-primary">
+                  Login
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+        <FooterBar />
+      </React.Fragment>
+    );
   }
 }
 
