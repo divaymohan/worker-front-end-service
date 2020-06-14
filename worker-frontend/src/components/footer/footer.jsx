7f29d7bd30cc02551dd964d85f24acdb9bb825a4
divaymohan <divaymohan05@gmail.com>
2020-06-14 17:32:26 +0530

    some changed into footer

--- worker-frontend/src/components/footer/footer.jsx
+++ worker-frontend/src/components/footer/footer.jsx
@@ -6,20 +6,12 @@ class FooterBar extends Component {
     return (
       <footer className="bg-dark jumbotron text-center mb-0 ">
         <div className="row">
+          <div className="col"></div>
           <div className="col">
-            <h3>
-              <span className="badge badge-light badge-pill">Github</span>
-            </h3>
+            <h3></h3>
           </div>
           <div className="col">
-            <h3>
-              <span className="badge badge-light badge-pill">LinkedIn</span>
-            </h3>
-          </div>
-          <div className="col">
-            <h3>
-              <span className="badge badge-light badge-pill">Twitter</span>
-            </h3>
+            <h3></h3>
           </div>
         </div>
       </footer>
