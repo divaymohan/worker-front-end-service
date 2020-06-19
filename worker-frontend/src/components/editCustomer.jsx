41489affc8475b432304c1ead50b816284bd5923
divaymohan <divaymohan05@gmail.com>
2020-06-19 13:21:04 +0530

    added edit state and schema ready to implement service

--- worker-frontend/src/components/editCustomer.jsx
+++ worker-frontend/src/components/editCustomer.jsx
@@ -4,57 +4,52 @@ import Form from "./common/form";
 import loginUser from "./../services/auth";
 import { toast } from "react-toastify";
 
-class Login extends Form {
+class Update extends Form {
   state = {
     data: {
-      firstName: "",
+      firstName: "Divay",
       lastName: "",
       middleName: "",
       userName: "",
       email: "",
       phoneNumber: "",
       password: "",
-      address: {
-        city: "",
-        area: "",
-        pin: "",
-      },
+      houseNumber: "hwy",
+      street: "",
+      country: "",
+      landmark: "",
+      city: "",
+      area: "",
+      pin: "",
       isSpecial: "",
     },
     errors: {},
   };
 
   schema = {
-    username: Joi.string().required().min(3).label("Username"),
+    userName: Joi.string().required().min(3).label("Username"),
+    firstName: Joi.string().required().min(3).label("First Name"),
+    lastName: Joi.string().required().min(3).label("Last Name"),
+    middleName: Joi.string().min(2).label("Middle Name"),
+    email: Joi.string().min(5).max(1000).label("Email"),
+    phoneNumber: Joi.number()
+      .min(1111111111)
+      .max(9999999999)
+      .required()
+      .label("Phone Number"),
+    houseNumber: Joi.number().min(1).required(),
+    street: Joi.string().min(3).max(1000),
+    country: Joi.string().min(3).max(100),
+    landmark: Joi.string().min(3).max(1000),
+    city: Joi.string().min(3).max(1000),
+    area: Joi.string().min(3).max(1000),
+    pin: Joi.number().min(111111).max(999999),
     password: Joi.string().required().min(3).label("Password"),
-    roll: Joi.string().required(),
+    isSpecial: Joi.string(),
   };
 
   doSubmit = async () => {
-    const { username, password, roll } = this.state.data;
-    if (roll.trim() !== "worker" && roll.trim() !== "customer") {
-      console.log(roll === "worker");
-      toast.error("Roll Must be worker or customer.", {
-        position: toast.POSITION.TOP_CENTER,
-      });
-      return;
-    }
-    try {
-      const { headers } = await loginUser(username, password, roll);
-      localStorage.setItem("token", headers["x-auth-token"]);
-      window.location = "/";
-
-      toast.success("Login Successfull..");
-      console.log("Submitted");
-    } catch (error) {
-      const errors = { ...this.state.errors };
-      errors.username = error.response.data;
-      this.setState({ errors });
-      toast.error(error.response.data, {
-        position: toast.POSITION.TOP_CENTER,
-      });
-      console.log(error);
-    }
+    //call server
   };
   render() {
     return (
@@ -62,14 +57,25 @@ class Login extends Form {
         <div className="container">
           <div className="card m-md-5 p-3 bg-light shadow-lg p-3 mb-5 bg-white rounded">
             <div className="card-title">
-              <h2>Login</h2>
+              <h2>Update</h2>
             </div>
             <div className="m-5">
               <form onSubmit={this.handleSubmit}>
+                {this.renderInput("firstName", "Fist Name*")}
+                {this.renderInput("lastName", "Last Name*")}
+                {this.renderInput("middleName", "Middle Name")}
+                {this.renderInput("email", "Email*")}
+                {this.renderInput("phoneNumber", "phoneNumber*")}
+                {this.renderInput("houseNumber", "House nmber*")}
+                {this.renderInput("city", "City*")}
+                {this.renderInput("street", "Street*")}
+                {this.renderInput("country", "Country*")}
+                {this.renderInput("landmark", "Landmark*")}
+                {this.renderInput("area", "Area")}
+                {this.renderInput("Pin", "Pin*")}
                 {this.renderInput("username", "Username")}
                 {this.renderInput("password", "Password", "password")}
-                {this.renderInput("roll", "Roll")}
-                {this.renderButton("Login")}
+                {this.renderButton("Update")}
               </form>
             </div>
           </div>
@@ -79,4 +85,4 @@ class Login extends Form {
   }
 }
 
-export default Login;
+export default Update;
--- worker-frontend/src/components/profile.jsx
+++ worker-frontend/src/components/profile.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Update from "./editCustomer";
 
 class Profile extends Component {
   state = {
@@ -13,27 +14,30 @@ class Profile extends Component {
   render() {
     return (
       <div className="container">
-        <div className="col-3">
-          <div className="m-0 shadow-lg p-3 mb-5 bg-white rounded">
-            <ul>
-              {this.state.classes.map((item) => (
-                <li
-                  onClick={() => this.onSelectSort(item)}
-                  key={item}
-                  className={
-                    this.state.Value === item
-                      ? "list-group-item active"
-                      : "list-group-item"
-                  }
-                  style={{ cursor: "pointer" }}
-                >
-                  <h6>
-                    <small>{item}</small>
-                  </h6>
-                </li>
-              ))}
-            </ul>
+        <div className="row">
+          <div className="col-3 m-5">
+            <div className="m-0 shadow-lg p-3 mb-5 bg-white rounded">
+              <ul>
+                {this.state.classes.map((item) => (
+                  <li
+                    onClick={() => this.onSelectSort(item)}
+                    key={item}
+                    className={
+                      this.state.Value === item
+                        ? "list-group-item active"
+                        : "list-group-item"
+                    }
+                    style={{ cursor: "pointer" }}
+                  >
+                    <h6>
+                      <small>{item}</small>
+                    </h6>
+                  </li>
+                ))}
+              </ul>
+            </div>
           </div>
+          <div className="col">{this.state.Value === "Edit" && <Update />}</div>
         </div>
       </div>
     );
